19090c15b30e06a22d047e570ecfd22c47e130e1
Karol <zalewski.karol@op.pl>
2024-06-06 17:45:16 +0200

    feat: modified order history page

--- frontend/src/components/orderhistory/OrderHistory.jsx
+++ frontend/src/components/orderhistory/OrderHistory.jsx
@@ -8,72 +8,75 @@ const OrderHistory = ({ orders }) => {
 
     const renderOrderHistory = () => {
         if (!auth.role) {
-            return <p>To see your Order History please log in</p>;
-        } else if (auth.role === "USER") {
+            return <p className="centered-text">To see your Order History please log in</p>;
+        }
+
+        if (orders.length === 0) {
+            return <p className="centered-text">Add new order</p>;
+        }
+
+        if (auth.role === "USER") {
             return (
                 <div className="order-history">
                     <h2 className="order-history-title">Order History</h2>
-                    {orders.length > 0 ? (
-                        <div className="order-list">
-                            {orders.map((order, index) => (
-                                <div className="order-item" key={index}>
-                                    <div className="order-date">Date: {order.date}</div>
-                                    <div className="order-details">
-                                        <div>
-                                            {order.orderItems.map((item, idx) => (
-                                                <div key={idx}>
-                                                    {item.quantity} x {item.product.name}
-                                                </div>
-                                            ))}
-                                        </div>
-                                        <div className="order-total">
-                                            Total: ${order.orderItems.reduce((acc, item) => acc + item.product.price * item.quantity, 0).toFixed(2)}
-                                        </div>
+                    <div className="order-list">
+                        {orders.map((order, index) => (
+                            <div className="order-item" key={index}>
+                                <div className="order-date">Date: {order.date}</div>
+                                <div className="order-details">
+                                    <div>
+                                        {order.orderItems.map((item, idx) => (
+                                            <div key={idx}>
+                                                {item.quantity} x {item.product.name}
+                                            </div>
+                                        ))}
+                                    </div>
+                                    <div className="order-total">
+                                        Total: ${order.orderItems.reduce((acc, item) => acc + item.product.price * item.quantity, 0).toFixed(2)}
                                     </div>
                                 </div>
-                            ))}
-                        </div>
-                    ) : (
-                        <p>No orders found for user.</p>
-                    )}
+                            </div>
+                        ))}
+                    </div>
                 </div>
             );
         } else if (auth.role === "ADMIN") {
             return (
                 <div className="order-history">
                     <h2 className="order-history-title">Orders</h2>
-                    {orders.length > 0 ? (
-                        <div className="order-list">
-                            {orders.map((order, index) => (
-                                <div className="order-item" key={index}>
-                                    <div className="order-date">Date: {order.date}</div>
-                                    <div className="order-details">
-                                        <div>
-                                            {order.orderItems.map((item, idx) => (
-                                                <div key={idx}>
-                                                    {item.quantity} x {item.product.name}
-                                                </div>
-                                            ))}
-                                        </div>
-                                        <div className="order-total">
-                                            Total: ${order.orderItems.reduce((acc, item) => acc + item.product.price * item.quantity, 0).toFixed(2)}
-                                        </div>
+                    <div className="order-list">
+                        {orders.map((order, index) => (
+                            <div className="order-item" key={index}>
+                                <div className="order-id">Order ID: {order.orderID}</div>
+                                <div className="client-id">Client ID: {order.user.id}</div>
+                                <div className="client-name">
+                                    Client Name: <span className="bold">{order.user.firstName} {order.user.lastName}</span>
+                                </div>
+                                <div className="order-date">Date: {order.date}</div>
+                                <div className="order-details">
+                                    <div>
+                                        {order.orderItems.map((item, idx) => (
+                                            <div key={idx}>
+                                                {item.quantity} x {item.product.name}
+                                            </div>
+                                        ))}
+                                    </div>
+                                    <div className="order-total">
+                                        Total: ${order.orderItems.reduce((acc, item) => acc + item.product.price * item.quantity, 0).toFixed(2)}
                                     </div>
                                 </div>
-                            ))}
-                        </div>
-                    ) : (
-                        <p>No orders found.</p>
-                    )}
+                            </div>
+                        ))}
+                    </div>
                 </div>
             );
         }
     };
 
     return (
-        <>
+        <div className="order-history-container">
             {renderOrderHistory()}
-        </>
+        </div>
     );
 };
 
--- frontend/src/pages/OrderHistoryPage.jsx
+++ frontend/src/pages/OrderHistoryPage.jsx
@@ -5,42 +5,42 @@ import api from '../services/api.jsx';
 import Header from '../components/header/Header.jsx';
 import { AuthContext } from "../auth/AuthContext.jsx";
 
-const OrderHistoryPage = ({ toggleCartVisibility, cartItems, isCartVisible, updateCartItems }) => {
+const OrderHistoryPage = () => {
     const auth = useContext(AuthContext);
     const [orders, setOrders] = useState([]);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
 
-    useEffect(() => {
-        const fetchOrders = async () => {
-            try {console.log(auth)
-                if (!auth.role || !auth.userId) {
-                    return;
-                }
-
-                let ordersData;
-                if (auth.role === "ADMIN") {
-                    ordersData = await api.getAllOrders();
-                } else if (auth.role === "USER") {
-                    ordersData = await api.getOrdersByUserId(auth.userId);
-                }
-                setOrders(ordersData);
+    const fetchData = async () => {
+        try {
+            if (!auth.role || !auth.userId) {
                 setLoading(false);
-            } catch (error) {
-                setError(error.message);
-                setLoading(false);
+                return;
             }
-        };
 
-        fetchOrders();
+            let ordersData;
+            if (auth.role === "ADMIN") {
+                ordersData = await api.getAllOrders();
+            } else if (auth.role === "USER") {
+                ordersData = await api.getOrdersByUserId(auth.userId);
+            }
+            setOrders(ordersData);
+            setLoading(false);
+        } catch (error) {
+            setError(error.message);
+            setLoading(false);
+        }
+    };
+
+    useEffect(() => {
+        fetchData();
     }, [auth.role, auth.userId]);
 
     return (
-        <Page pageTitle="Order History" cartItems={cartItems} isCartVisible={isCartVisible} updateCartItems={updateCartItems}>
-            <Header title="MarioLuigi" toggleCartVisibility={toggleCartVisibility}/>
+        <Page pageTitle="Order History">
+            <Header title="MarioLuigi" />
             {loading && <p>Loading...</p>}
-            {error && <p>{error}</p>}
-            {!loading && !error && <OrderHistory orders={orders} />}
+            {!loading && <OrderHistory orders={orders} error={error} />}
         </Page>
     );
 };
